cfcca06a959663d030e7cff4dccd6eeffdf0b010
f3kin <60875fil@gmail.com>
2024-08-31 13:51:59 +1000

    Working on signup page

---
+++ woof-band/app/dog-info/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const DogInfo: React.FC = () => {
+    return (
+        <div>
+            <h1>First, Tell us more about your Dog...</h1>
+        </div>
+    );
+};
+
+export default DogInfo;
