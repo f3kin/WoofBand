64f4d812695679efbf1157dd0e7506557c5a0fc4
f3kin <60875fil@gmail.com>
2024-08-31 16:30:55 +1000

    setup firebase, still configging data types

--- woof-band/app/api/register/route.ts
+++ woof-band/app/api/register/route.ts
@@ -1,10 +1,23 @@
 import { NextResponse } from 'next/server';
-import { nextTick } from 'process';
+import { db } from '../../../config/firebaseConfig';
+import { collection, addDoc } from "firebase/firestore"
 
 export async function POST(request: Request) {
     const data = await request.json();
 
-    console.log('Received data in ./app/api/register', data);
-
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+            userName: data.userName,
+            userEmail: data.userEmail,
+            userPhone: data.userPhone,
+            dogName: data.dogName,
+            dogBreed: data.dogBreed,
+        });
+    
+    console.log("Document written with ID: ", docRef.id);
     return NextResponse.json({ message: 'User registered successfully' });
+    } catch (e) {
+        console.error("Error adding document: ", e);
+        return NextResponse.json({ message: "Error register user" }, { status: 500 });
+    }
 }
